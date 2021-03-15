7089a16fd6675033a7ec6b036ad56ed07d9d4d73
Hamilakis Nicolas <nicolas.hamilakis@ens.fr>
2021-03-15 17:31:45 +0100

    testing pyproject.toml

--- bin/init.sh
+++ bin/init.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-# list of aliases to debug zerospeech server
-
-
+# source this file to work on the API
+conda activate zapi
+export ZR_ENV_FILE=test.env
 alias run_server="uvicorn zerospeech.api:app --reload --debug"
 alias zr="python -m zerospeech.admin"
-
-# todo: add ZR_ENV_FILE here?
