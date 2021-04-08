2579c2580582b17fd438f88ff3ce047f9fc323a1
Hamilakis Nicolas <nicolas.hamilakis@ens.fr>
2021-04-08 16:13:51 +0200

    update install & add auto packaging

--- setup.py
+++ setup.py
@@ -3,29 +3,11 @@
 import setuptools
 import codecs
 
+
 with codecs.open('requirements.txt', encoding='utf-8') as fp:
     requirements = fp.read()
 
-with codecs.open('README.md', encoding='utf-8') as fp:
-    readme = fp.read()
-
-# todo: add namespace for evaluation scripts (?)
-
+# run setup with setup.cfg
 setuptools.setup(
-    name='zerospeech',
-    description="Back-end for the Zerospeech Challenge",
-    long_description=readme,
-    packages=setuptools.find_packages(where='.'),
-    install_requires=requirements,
-    entry_points={
-        'console_scripts': [
-            'zr=zerospeech.admin.cli.main:run_cli',
-        ]
-    },
-    author='Nicolas Hamilakis, CoML team ENS/Inria Paris',
-    author_email='contanct@zerospeech.com',
-    license='GPL3',
-    url='https://zerospeech.com/',
-    long_description_content_type="text/markdown",
-    python_requires='>=3.7',
+    install_requires=requirements
 )
