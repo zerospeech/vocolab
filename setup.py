3e8ce29e7f9a4cde96cfc3beb36a6930ec9d25cc
Hamilakis Nicolas <nicolas.hamilakis@ens.fr>
2021-06-25 13:45:00 +0200

    bugfix: include config templates in installation

--- setup.py
+++ setup.py
@@ -9,17 +9,8 @@ with codecs.open('requirements.txt', encoding='utf-8') as fp:
 # run setup with setup.cfg
 setuptools.setup(
     install_requires=requirements,
-    include_package_data=True,
+    # include_package_data=True,
     package_data={
-        '': ["*.jinja2"],
-        'zerospeech_admin': [
-            'zerospeech/templates/emails/*.jinja2',
-            'zerospeech/templates/mattermost/*.jinja2',
-            'zerospeech/templates/pages/*.jinja2',
-            'zerospeech/templates/config/*.service',
-            'zerospeech/templates/config/*.socket',
-            'zerospeech/templates/config/*.wsgi',
-            'zerospeech/templates/config/*.conf',
-        ],
+        '': ['*.jinja2', '*.service', '*.socket', '*.wsgi', '*.conf', '*.env']
     },
 )
