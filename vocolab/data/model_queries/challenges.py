0e70b252e9da1ea744d43cf1428e8d7790ebe67f
Hamilakis Nicolas <nick.hamilakis562@gmail.com>
2023-02-03 13:48:25 +0100

    WIP: db clean-up for challenges/models/submissions (part 2)

--- vocolab/data/model_queries/challenges.py
+++ vocolab/data/model_queries/challenges.py
@@ -1,3 +1,4 @@
+import shlex
 from datetime import date
 from datetime import datetime
 from pathlib import Path
@@ -8,7 +9,7 @@ from pydantic import HttpUrl
 
 from vocolab.data import models, tables
 from vocolab.core import misc
-from ..base import zrDB, db_exc
+from ..db import zrDB, db_exc
 
 
 class EvaluatorItem(BaseModel):
@@ -22,6 +23,53 @@ class EvaluatorItem(BaseModel):
 
     class Config:
         orm_mode = True
+
+    @classmethod
+    async def add_or_update(cls, *, evl_item: models.cli.NewEvaluatorItem):
+        query = tables.evaluators_table.select().where(
+            tables.evaluators_table.c.label == evl_item.label
+        ).where(
+            tables.evaluators_table.c.host == evl_item.host
+        )
+        res = await zrDB.fetch_one(query)
+
+        if res is None:
+            await zrDB.execute(tables.evaluators_table.insert(), evl_item.dict())
+        else:
+            update_query = tables.evaluators_table.update().where(
+                tables.evaluators_table.c.id == res.id
+            ).values(
+                executor=evl_item.executor, script_path=evl_item.script_path,
+                executor_arguments=evl_item.executor_arguments
+            )
+            await zrDB.execute(update_query)
+
+    @classmethod
+    async def get(cls, by_id: str) -> Optional["EvaluatorItem"]:
+        query = tables.evaluators_table.select().where(
+            tables.evaluators_table.c.id == by_id
+        )
+        result = await zrDB.fetch_one(query)
+        if not result:
+            return None
+        return cls.parse_obj(result)
+    async def update_args(self, arg_list: List[str]):
+        query = tables.evaluators_table.update().where(
+            tables.evaluators_table.c.id == self.id
+        ).values(executor_arguments=shlex.join(arg_list))
+
+
+class EvaluatorList(BaseModel):
+    items: List[EvaluatorItem]
+
+    @classmethod
+    async def get(cls) -> "EvaluatorList":
+        query = tables.evaluators_table.select()
+        results = await zrDB.fetch_all(query)
+        if not results:
+            return cls(items=[])
+        return cls(items=results)
+
 
 
 class Challenge(BaseModel):
@@ -149,6 +197,94 @@ class Leaderboard(BaseModel):
 
     class Config:
         orm_mode = True
+
+    @classmethod
+    async def create(cls, ld_data: 'Leaderboard'):
+        query = tables.leaderboards_table.insert().values(
+            label=ld_data.label,
+            challenge_id=ld_data.challenge_id,
+            path_to=f"{ld_data.path_to}",
+            entry_file=ld_data.entry_file,
+            archived=ld_data.archived,
+            external_entries=f"{ld_data.external_entries}",
+            static_files=ld_data.static_files
+        )
+        try:
+            result = await zrDB.execute(query)
+            return result
+        except Exception as e:
+            db_exc.parse_user_insertion(e)
+
+    async def update_property(self, *, variable_name: str, value: Any, allow_parsing: bool = False):
+        """ Update a named property """
+        if not hasattr(self, variable_name):
+            raise ValueError(f'Class Leaderboard does not have a member called ! {variable_name}')
+
+        variable_type = type(getattr(self, variable_name))
+
+        if allow_parsing:
+            value = misc.str2type(value, variable_type)
+
+        if value is not None and not isinstance(value, variable_type):
+            raise ValueError(f"Leaderboard.{variable_name} should be of type {variable_type}")
+
+        if value is None:
+            if not self.__fields__.get(variable_name).allow_none:
+                raise ValueError(f'LeaderBoard.{variable_name} cannot be None/Null')
+        else:
+            if not isinstance(value, variable_type):
+                raise ValueError(f"Leaderboard.{variable_name} should be of type {variable_type}")
+
+        # set value
+        setattr(self, variable_name, value)
+
+        # Path is not supported by sqlite as a raw type
+        if variable_type == Path:
+            value = str(value)
+
+        query = tables.leaderboards_table.update().where(
+            tables.leaderboards_table.c.id == self.id
+        ).values({f"{variable_name}": str(value)})
+        try:
+            await zrDB.execute(query)
+        except Exception as e:
+            db_exc.parse_user_insertion(e)
+
+        return value
+
+
+    @classmethod
+    async def get(cls, leaderboard_id: int) -> Optional["Leaderboard"]:
+        query = tables.leaderboards_table.select().where(
+            tables.leaderboards_table.c.id == leaderboard_id
+        )
+        ld = await zrDB.fetch_one(query)
+        if ld is None:
+            return None
+        return cls.parse_obj(ld)
+
+
+class LeaderboardList(BaseModel):
+    items: List[Leaderboard]
+
+    @classmethod
+    async def get_all(cls) -> "LeaderboardList":
+        query = tables.leaderboards_table.select()
+        ld_list = await zrDB.fetch_all(query)
+        if not ld_list:
+            return cls(items=[])
+        return cls(items=ld_list)
+
+    @classmethod
+    async def get_by_challenge(cls, challenge_id: int) -> "LeaderboardList":
+        query = tables.leaderboards_table.select().where(
+            tables.leaderboards_table.c.challenge_id == challenge_id
+        )
+        ld_list = await zrDB.fetch_all(query)
+        if not ld_list:
+            return cls(items=[])
+        return cls(items=ld_list)
+
 
 
 class LeaderboardEntry:
